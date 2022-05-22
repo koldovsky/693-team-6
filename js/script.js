8b8c3e5db1adf7a846c1c354e0701ca423aa51cd
oleynikusss <oleynik.igor95@gmail.com>
2022-05-22 21:42:41 +0300

    fixed header

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+const bodyEl = document.querySelector(".body");
+
+btnNavEl.addEventListener("click", function () {
+  bodyEl.classList.toggle("lock-scroll");
+});
