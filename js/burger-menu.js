92eade0e64aaf9798bc7f6c3501fb4552c41e769
oleynikusss <oleynik.igor95@gmail.com>
2022-05-30 21:43:09 +0300

    fixed header and created burger-mune.js

---
+++ js/burger-menu.js
@@ -0,0 +1,15 @@
+(function() {
+const btnNavEl = document.querySelector(".nav-btn");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const bodyEl = document.querySelector(".body");
+
+btnNavEl.addEventListener("click", function () {
+  bodyEl.classList.toggle("lock-scroll");
+});
+
+})();
